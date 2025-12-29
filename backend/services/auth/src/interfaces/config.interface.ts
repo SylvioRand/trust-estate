d7f2437bd7ffc90fc2d3d0bd75943576bfadc117
Arthinew <dinandrianom@gmail.com>
2025-12-29 12:29:11 +0300

    Deleta schema for JWT_SECRET

--- backend/services/auth/src/interfaces/config.interface.ts
+++ backend/services/auth/src/interfaces/config.interface.ts
@@ -1,7 +1,5 @@
 export interface EnvConfigInterface {
 	PORT_AUTH_SERVICE: string
-	INTERNAL_SECRET: string
-	JWT_SECRET: string
 	JWT_REFRESH_SECRET: string
 	GOOGLE_CLIENT_ID: string
 	GOOGLE_CLIENT_SECRET: string
