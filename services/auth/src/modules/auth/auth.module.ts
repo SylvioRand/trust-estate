78fda599aa858bad50a1ef0e9e6d80517f639f80
srandria <>
2026-01-12 00:10:47 +0300

    Update listings service with zone validation and API endpoints

--- services/auth/src/modules/auth/auth.module.ts
+++ services/auth/src/modules/auth/auth.module.ts
@@ -8,19 +8,19 @@ import helmetPlugin from "../../plugin/helmet.plugin";
 import rateLimitPlugin from "../../plugin/rate-limit";
 
 export async function pluginRegister(app: FastifyInstance) {
-	await app.register(fastifyCookie, {
-		secret: app.config.COOKIE_SECRET,
-	});
-	await app.register(jwtPlugin);
-	await app.register(rateLimitPlugin);
-	await app.register(helmetPlugin);
-	await app.register(prismaPlugin);
-	await app.register(mailPlugin);
+  await app.register(fastifyCookie, {
+    secret: app.config.COOKIE_SECRET,
+  });
+  await app.register(jwtPlugin);
+  await app.register(rateLimitPlugin);
+  await app.register(helmetPlugin);
+  await app.register(prismaPlugin);
+  await app.register(mailPlugin);
 }
 
 export async function authRegister(app: FastifyInstance) {
-	await app.register(authRoutes, {prefix: "/auth"});
-	await app.register(emailAuthRoutes, {prefix: "/auth"})
-	await app.register(oathAuthRoutes, {prefix: "/auth"});
-	await app.register(authenticationInterne, {prefix: "/auth"});
+  await app.register(authRoutes, { prefix: "/auth" });
+  await app.register(emailAuthRoutes, { prefix: "/auth" })
+  await app.register(oathAuthRoutes, { prefix: "/auth" });
+  await app.register(authenticationInterne, { prefix: "/auth" });
 }
--- services/listings/src/app.ts
+++ services/listings/src/app.ts
@@ -1,22 +1,15 @@
 import fastify from 'fastify';
+import { listingRoutes } from './modules/listing/listing.controller';
 
 const app = fastify();
 
-app.register(async (fastify) => {
-  fastify.get('/health', (req, res) => {
-    res.send({ status: 'ok' });
-  });
-
-  fastify.get('/publish', (req, res) => {
-    res.send('BOnjour fastify');
-  });
-}, { prefix: '/listings' });
+app.register(listingRoutes, { prefix: '/listings' });
 
 app.listen({ port: 3002, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
-  console.log(`Server listening on ${address}`);
+  console.log(`listing Server listening on ${address}`);
 });
 
