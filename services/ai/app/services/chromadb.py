7f36d21ea9a80ff1f996e2e6d9ae903c1832fe46
aelison <aelison@student.42antananarivo.mg>
2026-02-04 13:57:49 +0300

    make the function generator_bloc() in asynchronous to get the error
    the error message is from the reader that is an asynchronous function,
    need to match to use the keyword "await"

--- services/ai/app/services/chromadb.py
+++ services/ai/app/services/chromadb.py
@@ -276,9 +276,7 @@ class ChromadbService:
         return new_result
 
     async def get_query(self, user_mssg, llm_service, sys_prompt, id_ref=None):
-        llm_parse_response = llm_service.generate_bloc_response(user_mssg, sys_prompt)
-
-        print(f"Hello there {llm_parse_response}")
+        llm_parse_response = await llm_service.generate_bloc_response(user_mssg, sys_prompt)
         datas = self.parse_json(llm_parse_response)
         if not datas:
             datas = {}
