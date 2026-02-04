9465eab2e39f1ba81e09f6112916fddedb024641
aelison <aelison@student.42antananarivo.mg>
2026-02-04 13:32:34 +0300

    AI chat: add limit length of a message to 5000 characters
    The LLM may have trouble if the message is too big

--- frontend/src/components/ChatTextArea.tsx
+++ frontend/src/components/ChatTextArea.tsx
@@ -45,8 +45,11 @@ export const ChatTextarea: React.FC<ChatTextareaProps> = ({
 			value={value}
 			rows={1}
 			placeholder={placeholder}
-			maxLength={2000}
-			onChange={(e) => onChange(e.target.value)}
+			onChange={(e) => {
+					if (e.target.value.length < 5000)
+						onChange(e.target.value);
+				}
+			}
 			onKeyDown={ (e) => {
 				onKeyDown(e);
 			}}
