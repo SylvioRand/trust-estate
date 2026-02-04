fbf00aaf4020c45e04c1f8acc4aeb8956416961b
Ny Hasina <djazejhasi@gmail.com>
2026-02-04 14:37:53 +0300

    update: display the user listings in the profile

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,109 +1,194 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import ActionButton from "../components/ActionButton";
 import ContentDivider from "../components/ContentDivider";
 import { CreateDateForMemberSince } from "../utils/Format";
-import BentoProperty from "../components/BentoProperty";
 import useDataProvider from "../provider/useDataProvider";
 import { VerifyUsersState } from "../hooks/VerifyUsersState";
+import type { ListingsTags, MyListingsData } from "../dataModel/modelListings";
+import { toast } from "react-toastify";
+import { TagsComponents } from "../components/TagsComponents";
 
-interface StatsProps {
-  value: number;
-  title: string;
+interface	MyListingsBentoProps {
+	data: MyListingsData;
 }
 
-const Stats: React.FC<StatsProps> = ({
-  value = 0,
-  title = "Title"
+const	MyListingsBento: React.FC<MyListingsBentoProps> = ({
+	data
 }) => {
-  return (
-    <div className="flex flex-col items-start justify-center
-			place-items-center
-			border border-background/25
-			rounded-xl
-			shadow-standard
-			p-2
-			flex-none
-			text-center
-			w-full
-			whitespace-nowrap
-			gap-3"
-    >
-      <div className="font-bold text-accent text-4xl
-				text-shadow-lg
-				truncate max-w-32"
-      >
-        {value}
-      </div>
-      <div className="font-light text-sm
-				truncate max-w-32">
-        {title}
-      </div>
-    </div>
-  );
-}
+	const	formatter = new Intl.NumberFormat("de-DE");
+	const	{ t } = useTranslation("common");
+	const	statusIcon: Record<"archived" | "active" | "blocked", string> = {
+		archived: "",
+		active: "",
+		blocked: ""
+	}
+	const	statusColors: Record<"archived" | "active" | "blocked", string> = {
+		archived: "var(--color-red-500)",
+		active: "transparent",
+		blocked: "var(--color-red-500)"
+	}
 
-interface ProfileSectionProps {
-  title: string;
-  children: React.ReactNode;
-}
-
-const ProfileSection: React.FC<ProfileSectionProps> = ({
-  title = "Title",
-  children
-}) => {
-  return (
-    <div
-      className="grid grid-cols-1 grid-rows-[auto_1fr]
-			place-items-center
-			gap-4
+	return (
+		<div
+		className="grid grid-cols-1 grid-rows-[auto_1fr]
+		rounded-xl
+		shadow-standard
+		relative
+		w-full"
+		>
+			<div
+			className="absolute top-2 left-2
+			flex flex-wrap gap-2
+			z-2
 			w-full"
-    >
-      <div className="grid grid-cols-[auto_1fr] grid-rows-1
-				gap-4
-				place-items-center
-				font-bold
+			>
+				{
+					data.tags && data.tags.map((value: ListingsTags, index: number) => {
+						return (
+							<TagsComponents
+							key={ index }
+							tags={ value }
+							/>
+						);
+					})
+				}
+			</div>
+
+			<div
+			className="flex items-center justify-center
+			relative
+			overflow-hidden
+			w-full aspect-square"
+			>
+				<div
+				className="absolute top-0 left-0
+				flex items-center justify-center
+				w-full h-full
+				select-none
+				rounded-t-xl
+				z-1"
+				style={{
+					backgroundColor: data.status !== "active" ? "color-mix(in srgb, black 50%, transparent)" : "transparent",
+				}}
+				>
+					<div
+					className="font-icon text-[64px]"
+					style={{
+						color: statusColors[data.status],
+						textShadow: `0px 0px 7px ${statusColors[data.status]}`
+					}}
+					>
+						{ statusIcon[data.status] }
+					</div>
+				</div>
+
+				<img
+				className="w-full h-full object-cover
+				rounded-t-xl"
+				src={ data.photos[0] }
+				alt="User listing house picture"
+				/>
+			</div>
+			<div
+			className="grid grid-cols-1 grid-rows-[auto_auto]
+			gap-3
+			place-items-start
+			rounded-b-xl
+			p-3
+			border-x border-b border-background/25"
+			>
+				<div
+				className="grid grid-cols-1 grid-rows-[auto_auto]
+				place-items-start
+				w-full">
+					<div
+					className="font-higuen font-bold
+					text-lg
+					truncate w-full"
+					>
+						{ data.title }
+					</div>
+
+					<div
+					className="flex items-center justify-center gap-1
+					w-full
+					text-sm"
+					>
+						<div className="font-icon"></div><div className="truncate w-full">{ data.zone }</div>
+					</div>
+				</div>
+
+				<div
+				className="grid grid-cols-1 grid-rows-[auto_auto]
 				w-full"
-      >
-        <div>
-          {title}
-        </div>
-        <div
-          className="w-full"
-        >
-          <ContentDivider
-            line_color="linear-gradient(to left,
-						transparent,
-						var(--color-background) 10%,
-						var(--color-background) 90%,
-						transparent)"
-          />
-        </div>
-      </div>
-      <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] grid-rows-1
-				gap-4
-				w-full"
-      >
-        {children}
-      </div>
-    </div>
-  );
+				>
+					<div
+					className="font-bold">
+						{ `${formatter.format(data.price)} Ar` }
+					</div>
+					<Link
+					className="w-full"
+					to={ `/property/listings?id=${data.id}` }
+					>
+						<ActionButton
+						title={ t("viewDetails") }
+						/>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 const ProfilePage: React.FC = () => {
   const navigate = useNavigate();
   const { userData, isConnected } = useDataProvider();
   VerifyUsersState();
-  const { t } = useTranslation(["profile", "listings"]);
+  const { t } = useTranslation(["profile", "listings", "common", "error"]);
+  const	[myListings, setMyListings] = useState<MyListingsData[]>([]);
 
   // Redirect if user is not connected
   if (isConnected !== null && isConnected === false)
 	navigate("/sign-in");
 
+  useEffect(() => {
+	const	getMyData = async () => {
+		try {
+			const	response = await fetch('/api/listings/mine?limit=100', {
+				method: 'GET',
+				credentials: 'include'
+			});
+			const	responseData = await response.json();
+
+			console.log(responseData);
+			
+			if (!response.ok)
+			{
+				if (responseData.details) {
+					const details: Record<string, string[]> = responseData.details as Record<string, string[]>;
+
+					for (const [key, value] of Object.entries(details)) {
+					for (let i = 0; i < value.length; i++)
+						toast.error(t(`error:${value[i]}`));
+					}
+				}
+				throw new Error(responseData.message);
+			}
+			setMyListings(responseData.data);
+		} catch (error) {
+			if (error instanceof Error && error.message !== "")
+				toast.error(t(`error:${error.message}`))
+		}
+	};
+
+	getMyData();
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-start
-			px-2 md:px-7 xl:px-64
+			px-4 md:px-7 xl:px-64
 			overflow-y-scroll
 			animate-fade-in
 			w-full h-screen"
@@ -158,10 +243,12 @@ const ProfilePage: React.FC = () => {
 					place-items-start
 					p-2
 					md:p-4
-					gap-4"
+					gap-0
+					xl:gap-4"
         >
           <Link
-            to="/profile/settings"
+		  className="w-full"
+		  to="/profile/settings"
           >
             <ActionButton
               icon=""
@@ -182,7 +269,7 @@ const ProfilePage: React.FC = () => {
       </div>
 
       <div
-        className="w-full"
+        className="w-full my-4"
       >
         <ContentDivider
           line_color="linear-gradient(to left,
@@ -193,64 +280,31 @@ const ProfilePage: React.FC = () => {
         />
       </div>
 
-      {/* This part may be implemented but not immediately, will wait for the back-end team first.
-
-			<div className="grid grid-cols-[repeat(auto-fill,minmax(120px,1fr))] grid-rows-1
-				p-4
-				place-items-center
-				gap-4
-				w-full"
-			>
-				{
-					Object.entries(userData?.sellerStats).map(([key, value]) => {
-						return (
-							<Stats
-								key={ key }
-								value={ value }
-								title={ t(`listings:section.stats.sellerStats.${key}`) }
-							/>
-						)
-					})
-				}
-			</div> */}
-
-      { // TODO: the data will be need to be fetched, so I will just comment it first
-			/* <div className="flex flex-col items-center justify-center
-				mt-12
-				gap-24
-				w-full p-2"
-			>
-				<ProfileSection
-					title={ t("section.myListings.title") }
-				>
-					{
-						listData.map((value: ListingsProps, index: number) => {
-							return (
-								<BentoProperty
-									key={ index }
-									propertyData={ value }
-								/>
-							);
-						})
-					}
-				</ProfileSection>
-
-				<ProfileSection
-					title={ t("section.favorites.title") }
-				>
-					{
-						listData.map((value: ListingsProps, index: number) => {
-							return (
-								<BentoProperty
-									key={ index }
-									propertyData={ value }
-								/>
-							);
-						})
-					}
-				</ProfileSection>
-			</div> */}
-
+	  {
+		myListings.length === 0 &&
+		<div>
+			{ t("noMyListings") }
+		</div>
+	  }
+	  <div
+	  className="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] grid-rows-1
+	  gap-3
+	  xl:px-0
+	  w-full"
+	  >
+	  	{
+			myListings && myListings.map((value: MyListingsData, index: number) => {
+				return (
+					<MyListingsBento
+					key={ index }
+					data={ value }
+					/>
+				);
+			})
+		}
+	  </div>
+	  <div
+	  className="w-full h-6 flex-none"></div>
     </div>
   );
 }
