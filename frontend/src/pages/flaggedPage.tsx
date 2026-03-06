66897f95fd5c04882ee33f28cae8fc8a255188c9
Ny Hasina <djazejhasi@gmail.com>
2026-03-06 15:23:42 +0300

    update: removed console.log in the ai section

--- frontend/src/pages/flaggedPage.tsx
+++ frontend/src/pages/flaggedPage.tsx
@@ -117,15 +117,12 @@ const FlaggedPage: React.FC = () => {
 	const { userData, isConnected } = useDataProvider();
 	const navigate = useNavigate();
 
-
 	useEffect(() => {
 		if (isConnected === null || isConnected !== null && isConnected === false
 			|| userData !== null && userData.role !== "MODERATOR"
-		)
+		) {
 			navigate("/sign-in");
-	}, [])
-
-	useEffect(() => {
+		}
 		const getActionHistory = async () => {
 			try {
 				const response = await fetch(`/api/moderator/listings/flagged`, {
