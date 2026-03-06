461dcd6b97455c74390a31d1ac90510164add3c0
Sylvio Randrianarisata <srandria@c1r8s9.42antananarivo.mg>
2026-03-06 20:21:54 +0300

    remove admin parts in seed.ts file

--- services/auth/prisma/seed.ts
+++ services/auth/prisma/seed.ts
@@ -1,12 +1,6 @@
 import { PrismaClient } from '@prisma/client';
-import dotenv from "dotenv"
 import bcrypt from "bcrypt"
 
-const EMAIL = process.env.EMAIL || "example@gmail.com"
-const FIRSTNAME = process.env.FIRSTNAME || "CASA"
-const LASTNAME = process.env.LASTNAME
-const PASSWORD = process.env.PASSWORD || "CASA!1234"
-const PHONE = process.env.PHONE
 
 const MODERATOR_EMAIL = process.env.EMAIL || "moderator@gmail.com"
 const MODERATOR_FIRSTNAME = process.env.FIRSTNAME || "srandria"
@@ -15,40 +9,15 @@ const MODERATOR_PASSWORD = process.env.PASSWORD || "CASA!1234"
 const MODERATOR_PHONE = process.env.PHONE
 
 
-
 const prisma = new PrismaClient();
 
 async function main() {
   const salt = await bcrypt.genSalt(12);
-  const passwordHash = await bcrypt.hash(PASSWORD, salt);
   const passwordModeratorHash = await bcrypt.hash(MODERATOR_PASSWORD, salt);
-  const adminCount = await prisma.user.count({
-    where: { role: "ADMIN" }
-  });
+
   const moderatorCount = await prisma.user.count({
     where: { role: "MODERATOR" }
   });
-
-
-  if (adminCount < 1) {
-
-    await prisma.user.upsert({
-      where: { email: EMAIL },
-      update: {
-        role: "ADMIN"
-      },
-      create: {
-        email: EMAIL,
-        emailVerified: true,
-        firstName: FIRSTNAME,
-        lastName: LASTNAME,
-        password: passwordHash,
-        phone: PHONE,
-        phoneVerified: true,
-        role: "ADMIN"
-      }
-    });
-  }
 
 
   if (moderatorCount < 1) {
@@ -70,8 +39,6 @@ async function main() {
       }
     });
   }
-
-
 }
 
 main()
